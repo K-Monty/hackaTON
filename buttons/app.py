160659b0059a168510d7c27a287cb53d13b7df12
K-Monty <kmgoh1995@gmail.com>
2022-07-03 03:41:16 +0200

    now just need to make it prettier and connect to backend

--- buttons/app.py
+++ buttons/app.py
@@ -10,4 +10,13 @@ def home():
 def calling():
     return render_template("calling.html")
 
+@app.route("/in_call")
+def in_call():
+    return render_template("call_fetched.html")
+
+@app.route("/call_ended")
+def call_ended():
+    return render_template("call_ended.html")
+
+
 app.run(debug = True)
