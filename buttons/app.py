721cfa57ba0afa0b86fb21b41c79274c556c707b
K.B <sherbet.apps@gmail.com>
2022-07-03 11:20:01 +0200

    modified the 'call in progress' page

--- buttons/app.py
+++ buttons/app.py
@@ -1,4 +1,5 @@
 from flask import Flask,render_template
+import requests
 
 app = Flask(__name__, template_folder='templates')
 
@@ -12,6 +13,9 @@ def calling():
 
 @app.route("/in_call")
 def in_call():
+    print("Starting payment channel with Tonfull... ")
+    res = requests.get("http://localhost:8888/startCall")
+    print("Tonefull server response: ",  res.content.decode('utf-8'))
     return render_template("call_fetched.html")
 
 @app.route("/call_ended")
